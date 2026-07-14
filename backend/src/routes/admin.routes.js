ed7e1740f80151b4905ef650f79724d9fc3688a6
unknown <S-GF-L1-D22@DIGI.LOCAL>
2026-07-14 22:19:25 +0300

    fix: branching

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 const router = express.Router();
-import {getAccountsAll, getDashboard} from "../controllers/Admin/Admin.controller.js"
-// Accounts (Tourists , Guide & Admins)
-// get all accounts
+import {getAccountsAll, getDashboard, getTrips} from "../controllers/Admin/Admin.controller.js"
+
 router.get("/dashboard",getDashboard)
 router.get("/accounts/:role/:page",getAccountsAll)
+
+router.get("/trips/:page",getTrips)
+
 export default router;
