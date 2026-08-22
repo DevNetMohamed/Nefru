a638ecda377011d8cec6a23e244fa58bcb26e103
Yousef-I-Ahmed <yousef8720@outlook.com>
2026-08-22 11:32:15 +0300

    revert

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -6,11 +6,6 @@ export const env = {
   nodeEnv: process.env.NODE_ENV || "development",
   port: Number(process.env.PORT) || 5000,
 
-
-  mongoUri:
-    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/nefru",
-
-
   jwtSecret: process.env.JWT_SECRET || "L6-210+1",
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || "7d",
 
