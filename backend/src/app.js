06ced9da9f46904963af7c3686b8d9cc1e2ce757
mohamed-h-eng <mohamedhany1615@gmail.com>
2026-08-22 10:58:39 +0300

    fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,13 +14,13 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// app.set("trust proxy", 1);
-// app.use(
-//   cors({
-//     origin: env.frontendUrl,
-//     credentials: true,
-//   }),
-// );
+app.set("trust proxy", 1);
+app.use(
+  cors({
+    origin: env.frontendUrl,
+    credentials: true,
+  }),
+);
 app.use(morgan("dev"));
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
