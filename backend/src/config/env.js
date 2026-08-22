820808f748fec3c8c8c9bfee278e034bbe7d66c7
Yousef-I-Ahmed <yousef8720@outlook.com>
2026-08-22 11:31:40 +0300

    revert

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -6,15 +6,11 @@ export const env = {
   nodeEnv: process.env.NODE_ENV || "development",
   port: Number(process.env.PORT) || 5000,
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
   mongoUri:
     process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/nefru",
 
-=======
->>>>>>> parent of 00a7b75 (Auth Refactor with Nodemailer)
-=======
->>>>>>> parent of 00a7b75 (Auth Refactor with Nodemailer)
+
   jwtSecret: process.env.JWT_SECRET || "L6-210+1",
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || "7d",
 
